de00342f98bcd32c285180d61c17c53740be1a0f
Yousung Yang <devfallingstar@gmail.com>
2019-12-02 19:11:13 +0900

    Simplify code ordering.py

--- ordering.py
+++ ordering.py
@@ -1,97 +1,88 @@
 import numpy as np
 
-def reOdering(image,size):
+
+def re_ordering(image, size):
     w_state = image[0][size - 1][0][0] - image[0][0][0][0]
     h_state = image[0][-1][0][1] - image[0][0][0][1]
 
     # true right or up or horizontal  false left or down or vertical
-
-    if(w_state>0):
-        right = False
-    else:
-        right = True
-
-    if(h_state>0):
-        up = False
-    else:
-        up = True
+    right = w_state <= 0
+    up = h_state <= 0
 
     w_state = image[0][1][0][0] - image[0][0][0][0]
     h_state = image[0][1][0][1] - image[0][0][0][1]
 
-    if(w_state>h_state):
+    if w_state > h_state:
         horizontal = True
     else:
         horizontal = False
 
+    if not right and not up and not horizontal:
+        object_point = np.zeros((size * size, 3), np.float32)
+        object_point[:, :2] = np.mgrid[0:size, 0:size].T.reshape(-1, 2)
 
-    if(right==False and up == False and horizontal == False):
-     objp = np.zeros((size * size, 3), np.float32)
-     objp[:, :2] = np.mgrid[0:size, 0:size].T.reshape(-1, 2)
+    elif not right and not up and horizontal:
+        object_point = np.zeros((size * size, 3), np.float32)
+        x, y = np.mgrid[0:size, 0:size]
+        n = np.array([y, x])
+        n = n.T.reshape(-1, 2)
+        object_point[:, :2] = n
+        object_point = [object_point]
 
-    elif(right==False and up == False and horizontal == True):
-     objp = np.zeros((size * size, 3), np.float32)
-     x,y = np.mgrid[0:size,0:size]
-     n = np.array([y,x])
-     n = n.T.reshape(-1,2)
-     objp[:,:2] = n
-     objp = [objp]
+    elif not right and up and not horizontal:
+        object_point = np.zeros((size * size, 3), np.float32)
+        n = np.mgrid[0:size, 0:size].T
+        for s in range(0, len(n)):
+            n[s] = np.sort(n[s], axis=0)[::-1]
+        object_point[:, :2] = n.reshape(-1, 2)
+        object_point = [object_point]
 
-    elif(right==False and up == True and horizontal == False):
-     objp = np.zeros((size*size,3),np.float32)
-     n = np.mgrid[0:size,0:size].T
-     for s in range(0,len(n)):
-      n[s] = np.sort(n[s],axis=0)[::-1]
-     objp[:,:2] = n.reshape(-1,2)
-     objp = [objp]
+    elif not right and up and horizontal:
+        object_point = np.zeros((size * size, 3), np.float32)
+        x, y = np.mgrid[0:size, 0:size]
+        n = np.array([y, x])
+        n = n.T
+        n = n[::-1]
+        object_point[:, :2] = n.reshape(-1, 2)
+        object_point = [object_point]
 
-    elif(right==False and up == True and horizontal == True):
-     objp = np.zeros((size * size, 3), np.float32)
-     x,y = np.mgrid[0:size,0:size]
-     n = np.array([y,x])
-     n = n.T
-     n = n[::-1]
-     objp[:,:2] = n.reshape(-1,2)
-     objp = [objp]
+    elif right and up and horizontal:
+        object_point = np.zeros((size * size, 3), np.float32)
+        x, y = np.mgrid[0:size, 0:size]
+        n = np.array([y, x])
+        n = n.T
+        n = n[::-1]
+        for s in range(0, len(n)):
+            n[s] = np.sort(n[s], axis=0)[::-1]
+        object_point[:, :2] = n.reshape(-1, 2)
+        object_point = [object_point]
 
-    elif(right==True and up == True and horizontal == True):
-     objp = np.zeros((size * size, 3), np.float32)
-     x,y = np.mgrid[0:size,0:size]
-     n = np.array([y,x])
-     n = n.T
-     n = n[::-1]
-     for s in range(0,len(n)):
-      n[s] = np.sort(n[s],axis=0)[::-1]
-     objp[:,:2] = n.reshape(-1,2)
-     objp = [objp]
+    elif right and up and not horizontal:
+        object_point = np.zeros((size * size, 3), np.float32)
+        n = np.mgrid[0:size, 0:size]
+        n = n.T
+        n = n[::-1]
+        for s in range(0, len(n)):
+            n[s] = np.sort(n[s], axis=0)[::-1]
+        object_point[:, :2] = n.reshape(-1, 2)
+        object_point = [object_point]
 
-
-    elif(right==True and up == True and horizontal == False):
-     objp = np.zeros((size * size, 3), np.float32)
-     n = np.mgrid[0:size,0:size]
-     n = n.T
-     n = n[::-1]
-     for s in range(0,len(n)):
-      n[s] = np.sort(n[s],axis=0)[::-1]
-     objp[:,:2] = n.reshape(-1,2)
-     objp = [objp]
-
-    elif(right==True and up == False and horizontal == False):
-     objp = np.zeros((size * size, 3), np.float32)
-     n = np.mgrid[0:size,0:size]
-     n = n.T
-     n = n[::-1]
-     objp[:,:2] = n.reshape(-1,2)
-     objp = [objp]
+    elif right and not up and not horizontal:
+        object_point = np.zeros((size * size, 3), np.float32)
+        n = np.mgrid[0:size, 0:size]
+        n = n.T
+        n = n[::-1]
+        object_point[:, :2] = n.reshape(-1, 2)
+        object_point = [object_point]
 
     else:
-      objp = np.zeros((size * size, 3), np.float32)
-      x,y = np.mgrid[0:size,0:size]
-      n = np.array([y,x])
-      n = n.T
-      for s in range(0,len(n)):
-       n[s] = np.sort(n[s],axis=0)[::-1]
-      objp[:,:2] = n.reshape(-1,2)
-      objp = [objp]
+        object_point = np.zeros((size * size, 3), np.float32)
+        x, y = np.mgrid[0:size, 0:size]
+        n = np.array([y, x])
+        n = n.T
+        for s in range(0, len(n)):
+            n[s] = np.sort(n[s], axis=0)[::-1]
+        object_point[:, :2] = n.reshape(-1, 2)
+        object_point = [object_point]
 
-    return objp
+    return object_point
